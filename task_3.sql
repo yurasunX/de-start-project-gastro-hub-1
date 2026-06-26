5fdf381adb64dc94834a803609074c51a21388f1
yurasunX <suhininy@gmail.com>
2026-06-27 00:44:58 +0200

    Add SQL query to retrieve top 3 cafes by manager changes

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,13 @@
-/*добавьте сюда запрос для решения задания 3*/
+SELECT
+    r.cafe_name,
+    COUNT(w.manager_uuid) - 1 AS manager_change_count
+FROM cafe.restaurant_manager_work_dates w
+JOIN cafe.restaurants r
+    ON w.restaurant_uuid = r.restaurant_uuid
+GROUP BY
+    r.restaurant_uuid,
+    r.cafe_name
+ORDER BY
+    manager_change_count DESC,
+    r.cafe_name
+LIMIT 3;
