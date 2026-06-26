4e7ae355f866f46e2c2aaab22d0419d5367bdf42
yurasunX <suhininy@gmail.com>
2026-06-27 00:37:17 +0200

    Create view for top 3 restaurants by average check

--- task_1.sql
+++ task_1.sql
@@ -1,1 +1,31 @@
-/*добавьте сюда запрос для решения задания 1*/
+CREATE OR REPLACE VIEW cafe.top_3_restaurants_by_avg_check AS
+WITH restaurant_avg_check AS (
+    SELECT
+        r.cafe_name,
+        r.type,
+        ROUND(AVG(s.avg_check), 2) AS avg_check
+    FROM cafe.sales s
+    JOIN cafe.restaurants r
+        ON s.restaurant_uuid = r.restaurant_uuid
+    GROUP BY
+        r.restaurant_uuid,
+        r.cafe_name,
+        r.type
+),
+ranked_restaurants AS (
+    SELECT
+        cafe_name,
+        type,
+        avg_check,
+        ROW_NUMBER() OVER (
+            PARTITION BY type
+            ORDER BY avg_check DESC
+        ) AS row_number
+    FROM restaurant_avg_check
+)
+SELECT
+    cafe_name,
+    type,
+    avg_check
+FROM ranked_restaurants
+WHERE row_number <= 3;
