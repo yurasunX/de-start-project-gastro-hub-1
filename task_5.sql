2d390d558eb63f071e0f5b039fc09fcae8f41099
yurasunX <suhininy@gmail.com>
2026-06-27 00:39:26 +0200

    Implement query to find highest priced pizza per cafe
    Added a SQL query to retrieve the most expensive pizza from each pizzeria.

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,30 @@
-/*добавьте сюда запрос для решения задания 5*/
+WITH menu_cte AS (
+    SELECT
+        r.cafe_name,
+        'Пицца' AS dish_type,
+        pizza.key AS pizza_name,
+        pizza.value::int AS price
+    FROM cafe.restaurants r
+    CROSS JOIN LATERAL json_each_text((r.menu -> 'Пицца')::json) AS pizza(key, value)
+    WHERE r.type = 'pizzeria'
+),
+menu_with_rank AS (
+    SELECT
+        cafe_name,
+        dish_type,
+        pizza_name,
+        price,
+        ROW_NUMBER() OVER (
+            PARTITION BY cafe_name
+            ORDER BY price DESC
+        ) AS price_rank
+    FROM menu_cte
+)
+SELECT
+    cafe_name,
+    dish_type,
+    pizza_name,
+    price
+FROM menu_with_rank
+WHERE price_rank = 1
+ORDER BY cafe_name;
